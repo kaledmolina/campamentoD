1d4534313f2759ffbb531074d8b0fdef5c0d28a8
kaledmolina <kaledmoly@gmail.com>
2026-02-04 08:53:16 -0500

    feat: Add CSRF token meta tag to base layouts.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
     <title>INVESTI2 - Campamento Juvenil 2026</title>
 
     <!-- Tailwind CSS -->
